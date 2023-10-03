3fcf96323c88ad7faa745557e517d31cb7f7e416
rasmi <rasminoel@gmail.com>
2023-10-03 10:04:11 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+// writing a POST request
+const submitData = (name, email) => {
+    const body = {
+        name: 'Steve',
+        email: 'steve@steve.com'
+    };
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(body)
+    };
+    return fetch("http://localhost:3000/users", configObj)
+        .then(resp => resp.json())
+        .then(json => {
+            document.body.innerHTML = json["id"]
+        })
+        .catch(error => {
+            document.body.innerHTML = error.message
+        })
+};
